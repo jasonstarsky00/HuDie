60ff5227e8e4dc886820c5d7b6c5dfe0f595f36e
jason <jasonstarsky00@gmail.com>
2020-10-10 18:16:02 +0800

    首页定稿

--- 原型/PPUI/js/index.js
+++ 原型/PPUI/js/index.js
@@ -16,34 +16,34 @@ $(document).ready(function(){
 
 
 
-//鼠标移入事件
-$(".imgright").mouseover(function(){
-    //移动的距离
-    var id = $(this).attr("data-imgid");
-    var height = $("#imgshow"+id).height()-400;
-    //移动总毫秒时间
-    var time = height/10;
-    if(height>0){
-        $("#imgshow"+id).stop().animate({top:'-'+height+'px'},time*50);
-    }
+// //鼠标移入事件
+// $(".imgright").mouseover(function(){
+//     //移动的距离
+//     var id = $(this).attr("data-imgid");
+//     var height = $("#imgshow"+id).height()-400;
+//     //移动总毫秒时间
+//     var time = height/10;
+//     if(height>0){
+//         $("#imgshow"+id).stop().animate({top:'-'+height+'px'},time*50);
+//     }
    
- });
+//  });
 
 
-$(".imgleft").mouseover(function(){
-    //移动的距离
-    var id = $(this).attr("data-imgid");
-    var top = Math.abs(parseInt($("#imgshow"+id).css("top")))
-    //移动总毫秒时间
-    var time = top/10;
-    $("#imgshow"+id).stop().animate({top:'0px'},time*50);
-});
-//鼠标移出事件
-$(".imgright").mouseout(function(){
-    var id = $(this).attr("data-imgid");
-    $("#imgshow"+id).stop()
-});
-$(".imgleft").mouseout(function(){
-    var id = $(this).attr("data-imgid");
-    $("#imgshow"+id).stop()
-});
+// $(".imgleft").mouseover(function(){
+//     //移动的距离
+//     var id = $(this).attr("data-imgid");
+//     var top = Math.abs(parseInt($("#imgshow"+id).css("top")))
+//     //移动总毫秒时间
+//     var time = top/10;
+//     $("#imgshow"+id).stop().animate({top:'0px'},time*50);
+// });
+// //鼠标移出事件
+// $(".imgright").mouseout(function(){
+//     var id = $(this).attr("data-imgid");
+//     $("#imgshow"+id).stop()
+// });
+// $(".imgleft").mouseout(function(){
+//     var id = $(this).attr("data-imgid");
+//     $("#imgshow"+id).stop()
+// });
